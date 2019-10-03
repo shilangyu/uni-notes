2e2f8adc1c3f6186208d1196a30f420f0935b574
shilangyu <xmarcinmarcin@gmail.com>
2019-10-03 14:47:09 +0200

    forcing copy

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 Get-ChildItem -Attributes directory -Exclude docs | ForEach-Object {
-	Copy-Item $_ -Filter *.html -Destination docs -Recurse
+	Copy-Item $_ -Filter *.html -Destination docs -Recurse -Force
 }
 
 git add docs
