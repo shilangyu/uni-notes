571b3229fc178d4044860e11ed2555ced605e890
shilangyu <xmarcinmarcin@gmail.com>
2023-01-17 22:57:42 +0100

    Generate intermediate folder indexes

--- .github/workflows/generate.jsx
+++ .github/workflows/generate.jsx
@@ -1,16 +1,18 @@
 import renderToString from "https://cdn.skypack.dev/preact-render-to-string";
-import { h } from "https://cdn.skypack.dev/preact@^10.4.4";
+import { h } from "https://cdn.skypack.dev/preact@^10.11.3";
 
 const React = { createElement: h }; // I don't want to create a jsconfig just to change the jsx factory...
 
-await Deno.remove("docs", { recursive: true }).catch(() => {});
-await Deno.mkdir("docs");
+const outFolder = "docs";
+
+await Deno.remove(outFolder, { recursive: true }).catch(() => {});
+await Deno.mkdir(outFolder);
 
 /// return a tuple of all the markdown files and directories in a given path
 /// respects internal excluded directories
 /// everything is sorted alphabetically
 function filesAndDirs(path) {
-  const excludedDirs = new Set([".git", "docs", ".vscode", ".github"]);
+  const excludedDirs = new Set([".git", outFolder, ".vscode", ".github"]);
   const files = [];
   const dirs = [];
 
@@ -29,6 +31,7 @@ function filesAndDirs(path) {
 }
 
 const pandocExec = [];
+const folderIndexes = [];
 
 /// recursively generates html files with pandoc and creates a jsx tree of a table of contents
 function Summary({ path }) {
@@ -36,7 +39,7 @@ function Summary({ path }) {
   const base = path + "/";
 
   for (const dirname of dirs) {
-    Deno.mkdirSync("docs/" + base + dirname);
+    Deno.mkdirSync(outFolder + "/" + base + dirname);
   }
 
   pandocExec.push(
@@ -49,15 +52,40 @@ function Summary({ path }) {
           "--number-sections",
           "--katex",
           "-o",
-          "docs/" + base + filename.replace(/md$/, "html"),
+          outFolder + "/" + base + filename.replace(/md$/, "html"),
           "--metadata",
           `pagetitle=${filename.slice(0, -3)}`,
           "-H",
           ".github/workflows/headers.html",
         ],
       }).status()
-    ),
+    )
   );
+
+  // special case: dont write to the top level index, that is written by <Summary /> itself
+  if (path !== ".") {
+    folderIndexes.push([
+      outFolder + "/" + base + "index.html",
+      <Helmet title={base}>
+        <a href="..">Go up</a>
+
+        <ul>
+          {dirs.map((dirname) => (
+            <li>
+              <a href={dirname}>{dirname}</a>
+            </li>
+          ))}
+          {files.map((filename) => (
+            <li>
+              <a href={filename.replace(/md$/, "html")}>
+                {filename.replace(/\.md$/, "")}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </Helmet>,
+    ]);
+  }
 
   return (
     <ul>
@@ -78,34 +106,47 @@ function Summary({ path }) {
   );
 }
 
-await Deno.writeTextFile(
-  "docs/index.html",
-  renderToString(
+const headerContent = await Deno.readTextFile(".github/workflows/headers.html");
+
+function Helmet({ title, children }) {
+  return (
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title>Uni notes</title>
+        <title>{title}</title>
         <span
           dangerouslySetInnerHTML={{
-            __html: await Deno.readTextFile(".github/workflows/headers.html"),
+            __html: headerContent,
           }}
-        >
-        </span>
+        ></span>
       </head>
-      <body>
-        Available notes:
-        <Summary path="." />
-        <footer>
-          Pages are auto-generated, if you see any problems please open an issue
-          on <a href="https://github.com/shilangyu/uni-notes">GitHub</a>
-          <br />
-          Last update: {new Date().toUTCString()}
-        </footer>
-      </body>
-    </html>,
-  ),
+      <body>{children}</body>
+    </html>
+  );
+}
+
+await Deno.writeTextFile(
+  outFolder + "/index.html",
+  renderToString(
+    <Helmet title="Uni notes">
+      Available notes:
+      <Summary path="." />
+      <footer>
+        Pages are auto-generated, if you see any problems please open an issue
+        on <a href="https://github.com/shilangyu/uni-notes">GitHub</a>
+        <br />
+        Last update: {new Date().toUTCString()}
+      </footer>
+    </Helmet>
+  )
+);
+
+await Promise.all(
+  folderIndexes.map(([filename, children]) =>
+    Deno.writeTextFile(filename, renderToString(children))
+  )
 );
 
 await Promise.all(pandocExec);
